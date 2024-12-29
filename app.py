8704e79211626e1398812cbb38f117bd09022ce3
Tnt-Projects24 <tnt-projects@outlook.com>
2024-12-29 05:43:20 +0000

    Automatic commit before Assistant edits

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, jsonify, render_template
-
+print ("hello")
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+    #return 'index.html'
+
+@app.route('/inventory')
+def inventory():
+    items =[
+
+      {'id':1, 'name':'ford',   'year':2024, 'price':1000},
+      {'id':2, 'name':'tesla',  'year':2022, 'price':1000},
+      {'id':3, 'name':'toyota', 'year':2025, 'price':1020}
+    ]
+    return render_template('inventory.html',items=items)
+    #return 'index.html'
+
+if (__name__ == '__main__'):
+  app.run(host="0.0.0.0", port=5000,debug=True)
